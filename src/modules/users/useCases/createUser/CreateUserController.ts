d971db7d0a209d935678a61dc09ebeeae2ff2008
julianojstavares <julianojstavares@outlook.com>
2022-08-04 17:11:49 -0300

    Setup project

--- src/modules/users/useCases/createUser/CreateUserController.ts
+++ src/modules/users/useCases/createUser/CreateUserController.ts
@@ -3,11 +3,11 @@ import { Response, Request } from "express";
 import { CreateUserUseCase } from "./CreateUserUseCase";
 
 class CreateUserController {
-  constructor(private createUserUseCase: CreateUserUseCase) {}
+    constructor(private createUserUseCase: CreateUserUseCase) {}
 
-  handle(request: Request, response: Response): Response {
-    // Complete aqui
-  }
+    handle(request: Request, response: Response): Response {
+        // Complete aqui
+    }
 }
 
 export { CreateUserController };
